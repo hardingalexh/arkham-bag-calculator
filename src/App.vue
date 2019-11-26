117bf3f9ddc1cc5f1aa549f71d83195ca0abf4f3
Alex Harding <alexharding@rti.org>
2019-11-26 15:01:56 -0500

    making body height full height for large monitors

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@
                 <ul>
                   <li id="chaosBagTab" :class="tab === 'chaosBag' ? 'is-active' : ''">
                     <a @click="tab = 'chaosBag'">Chaos Bag</a>
-                  
+                  </li>
                   <li id="cardEffectsTab" :class="tab === 'cardEffects' ? 'is-active' : ''">
                     <a @click="tab = 'cardEffects'">Card Effects</a>
                   </li>
@@ -133,15 +133,42 @@
         </header>
         <section class="modal-card-body">
           <div class="card-content">
-            <p>The panel on the left side of the screen titled <strong>Configure Chaos Bag and Card Effects</strong> is where you set all of the variables. This panel has a table that represents your chaos bag - for each token, you define how many copies are in the bag, and what modifier each token applies to your test result. To use this tool, try the following:</p>
+            <p>
+              The panel on the left side of the screen titled
+              <strong>Configure Chaos Bag and Card Effects</strong> is where you set all of the variables. This panel has a table that represents your chaos bag - for each token, you define how many copies are in the bag, and what modifier each token applies to your test result. To use this tool, try the following:
+            </p>
             <br />
-            <p><strong>1.</strong> Choose your campaign and difficulty from the <strong>Choose Campaign</strong> dropdown. This will set the quantities of tokens to that campaign/difficulty.</p>
-            <p><strong>2.</strong> Manually add or remove any tokens that have been added/removed by your campaign by changing the dropdown in the <strong>Quantity</strong> column for that token.</p>
-            <p><strong>3.</strong> Define the symbol tokens' modifiers by changing the value of the dropdown in the <strong>Effect</strong> column for that token.</p>
-            <p><small>For example, if the skull token is worth -X where X is the number of ghouls at your location, and there are 3 ghouls at your location, set the <strong>Effect</strong> for skull to -3.</small></p>
-            <p><strong>4.</strong> Set any other token effects, such as <strong>Draw Again</strong> or <strong>Automatically Fails</strong>
-            <p><strong>5.</strong> Choose a character from the <strong>Choose Character</strong> dropdown. This will set the effect of the Elder Sign token, as well as apply and specific effects for investigators like Jim Culver or Father Mateo. If the chosen character's elder sign ability is variable, you may need to manually specify their effect.</p>
-            <p><strong>6.</strong> Select the <strong>Card Effects</strong> tab to select any cards you may be using for this skill test.</p>
+            <p>
+              <strong>1.</strong> Choose your campaign and difficulty from the
+              <strong>Choose Campaign</strong> dropdown. This will set the quantities of tokens to that campaign/difficulty.
+            </p>
+            <p>
+              <strong>2.</strong> Manually add or remove any tokens that have been added/removed by your campaign by changing the dropdown in the
+              <strong>Quantity</strong> column for that token.
+            </p>
+            <p>
+              <strong>3.</strong> Define the symbol tokens' modifiers by changing the value of the dropdown in the
+              <strong>Effect</strong> column for that token.
+            </p>
+            <p>
+              <small>
+                For example, if the skull token is worth -X where X is the number of ghouls at your location, and there are 3 ghouls at your location, set the
+                <strong>Effect</strong> for skull to -3.
+              </small>
+            </p>
+            <p>
+              <strong>4.</strong> Set any other token effects, such as
+              <strong>Draw Again</strong> or
+              <strong>Automatically Fails</strong>
+            </p>
+            <p>
+              <strong>5.</strong> Choose a character from the
+              <strong>Choose Character</strong> dropdown. This will set the effect of the Elder Sign token, as well as apply and specific effects for investigators like Jim Culver or Father Mateo. If the chosen character's elder sign ability is variable, you may need to manually specify their effect.
+            </p>
+            <p>
+              <strong>6.</strong> Select the
+              <strong>Card Effects</strong> tab to select any cards you may be using for this skill test.
+            </p>
           </div>
         </section>
         <footer class="modal-card-foot">
@@ -361,7 +388,13 @@ export default {
 };
 </script>
 <style>
+html {
+  height: 100%;
+  min-height: 100%;
+}
 body {
+  min-height: 100%;
+
   font-size: 0.75em !important;
   font-weight: 400 !important;
   line-height: 1.5 !important;
